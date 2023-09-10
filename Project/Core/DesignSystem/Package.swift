aefb7a572bd2d981aaaf0c51528e81d9fba50d6a
승찬 <anstmdcks12@naver.com>
2023-09-10 19:02:14 +0900

    feat: Core.DesignSystem 모듈 추가

---
+++ Project/Core/DesignSystem/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "DesignSystem",
+  platforms: [
+    .iOS(.v16),
+  ],
+  products: [
+    .library(
+      name: "DesignSystem",
+      targets: ["DesignSystem"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "DesignSystem",
+      dependencies: []),
+    .testTarget(
+      name: "DesignSystemTests",
+      dependencies: ["DesignSystem"]),
+  ]
+)
