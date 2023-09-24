73182dc9d5da128d0d10731a2be7859cfae29c55
승찬 <anstmdcks12@naver.com>
2023-09-24 14:37:31 +0900

    fix: swift package format

--- Project/Feature/Movie/Sources/Movie/Source/Feature/MovieHome/UIComponent/MovieHome+ItemListComponent.swift
+++ Project/Feature/Movie/Sources/Movie/Source/Feature/MovieHome/UIComponent/MovieHome+ItemListComponent.swift
@@ -1,6 +1,8 @@
-import Foundation
 import Domain
+import Foundation
 import SwiftUI
+
+// MARK: - MovieHomePage.ItemListComponent
 
 extension MovieHomePage {
   struct ItemListComponent {
@@ -13,7 +15,7 @@ extension MovieHomePage.ItemListComponent {
   private func formatDate(_ dateString: String) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd"
-    
+
     if let date = dateFormatter.date(from: dateString) {
       dateFormatter.dateFormat = "M/d/yy"
       return dateFormatter.string(from: date)
@@ -23,12 +25,14 @@ extension MovieHomePage.ItemListComponent {
   }
 }
 
+// MARK: - MovieHomePage.ItemListComponent + View
+
 extension MovieHomePage.ItemListComponent: View {
   var body: some View {
     ScrollView {
       LazyVStack {
-        ForEach(viewState.itemList) { item  in
-          
+        ForEach(viewState.itemList) { item in
+
           HStack(spacing: 16) {
             Asset.spongeBob.swiftUIImage
               .resizable()
@@ -36,16 +40,15 @@ extension MovieHomePage.ItemListComponent: View {
               .clipShape(RoundedRectangle(cornerRadius: 10))
               .overlay(
                 RoundedRectangle(cornerRadius: 10)
-                  .stroke(.black, lineWidth: 1)
-              )
+                  .stroke(.black, lineWidth: 1))
               .shadow(radius: 10)
-            
-            VStack (alignment: .leading, spacing: 8){
+
+            VStack(alignment: .leading, spacing: 8) {
               Text(item.title)
                 .font(.headline)
                 .fontWeight(.regular)
                 .foregroundColor(.customYellowColor)
-              
+
               HStack {
                 Circle()
                   .trim(from: 0, to: item.voteAverage / 10)
@@ -54,25 +57,24 @@ extension MovieHomePage.ItemListComponent: View {
                   .rotationEffect(.degrees(-90))
                   .frame(width: 40, height: 40)
                   .foregroundColor(Color.lineColor(item.voteAverage))
-                  .shadow(color:Color.lineColor(item.voteAverage), radius: 5, x: 0, y: 0)
-                  .overlay (
+                  .shadow(color: Color.lineColor(item.voteAverage), radius: 5, x: 0, y: 0)
+                  .overlay(
                     Text("\(Int(item.voteAverage * 10))%")
-                      .font(.system(size: 10))
-                  )
-                
+                      .font(.system(size: 10)))
+
                 Text(formatDate(item.releaseDate))
                   .font(.subheadline)
               }
-              
+
               Text(item.overView)
                 .font(.callout)
                 .foregroundColor(Color.gray)
                 .multilineTextAlignment(.leading)
                 .lineLimit(3)
             }
-            
+
             Spacer()
-            
+
             Image(systemName: "chevron.right")
               .resizable()
               .frame(width: 8, height: 12)
@@ -80,7 +82,7 @@ extension MovieHomePage.ItemListComponent: View {
               .padding(.trailing, 16)
           } // Hstack
           .padding(.vertical, 8)
-          
+
           Divider()
             .padding(.leading, 144)
             .onAppear {
@@ -99,17 +101,21 @@ extension MovieHomePage.ItemListComponent: View {
   }
 }
 
+// MARK: - MovieHomePage.ItemListComponent.ViewState
+
 extension MovieHomePage.ItemListComponent {
   struct ViewState: Equatable {
     let itemList: [MovieItem]
     let lastID: Int
-    
+
     init(rawValue: [MovieDomain.MovieList.Response.ResultItem]) {
       itemList = rawValue.map(MovieItem.init(rawValue:))
-      self.lastID = rawValue.last?.id ?? .zero
+      lastID = rawValue.last?.id ?? .zero
     }
   }
 }
+
+// MARK: - MovieHomePage.ItemListComponent.ViewState.MovieItem
 
 extension MovieHomePage.ItemListComponent.ViewState {
   struct MovieItem: Equatable, Identifiable {
@@ -118,25 +124,30 @@ extension MovieHomePage.ItemListComponent.ViewState {
     let voteAverage: Double
     let releaseDate: String
     let overView: String
-    
+
     init(rawValue: MovieDomain.MovieList.Response.ResultItem) {
-      self.id = rawValue.id
-      self.title = rawValue.title
-      self.voteAverage = rawValue.voteAverage
-      self.releaseDate = rawValue.releaseDate
-      self.overView = rawValue.overview
+      id = rawValue.id
+      title = rawValue.title
+      voteAverage = rawValue.voteAverage
+      releaseDate = rawValue.releaseDate
+      overView = rawValue.overview
     }
-    
+
   }
 }
 
 extension Color {
+
+  // MARK: Public
+
   public static var customYellowColor = Color(red: 0.75, green: 0.6, blue: 0.2)
-  
+
   public static var customGreenColor = Color(red: 0.45, green: 0.64, blue: 0.62)
-  
+
   public static var customBgColor = Color(red: 0.94, green: 0.94, blue: 0.96)
-  
+
+  // MARK: Internal
+
   static func lineColor(_ voteAverage: Double) -> Color {
     if voteAverage >= 7.5 {
       return .green
@@ -147,4 +158,3 @@ extension Color {
     }
   }
 }
-
